de9599b2fe59c9955a54739a4b2ae624461079ca
Vaculya <95585403+Vaculya@users.noreply.github.com>
2022-09-09 13:48:05 +0300

    Add files via upload
    попытка 2

---
+++ script/js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.baner__inner').slick({
+        // arrows: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        cssEase: 'linear'
+    });
+});
